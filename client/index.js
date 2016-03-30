e0d7792b05dac980e06466f4860a2074eb9e4afe
Dustin Hollis <dustin.hollis@live.com.au>
2016-03-30 15:41:32 +1100

    Update List was created

--- client/index.js
+++ client/index.js
@@ -7,21 +7,36 @@ myApp.service('HistoryService' , function ($http) {
 		var url = baseUrl + "saveCurrent"
 		return $http.post(url, {"word": newWord})
 	}
+
+	this.getSaved = function () {
+		var url = baseUrl + "getSaved"
+		return $http.get(url)
+	}
 })
 
 myApp.controller('MyController', function ($scope, HistoryService) {
 	$scope.newWord = 'cat'
+	$scope.words = []
 
 	$scope.saveThisWord = function () {
 		HistoryService.saveWord( $scope.newWord )
 		.then(saveSuccess, error)
 	}
 
+	$scope.getSavedWords = function () {
+		HistoryService.getSaved()
+		.then(loadSuccess, error)
+	}
+
+	function loadSuccess (json) {
+		$scope.words = json.data
+	}
+
 	function saveSuccess (json) {
 		console.log(json)
 	}
 
-	function error (json) {
-		console.log(json)
+	function error (err) {
+		console.log(err)
 	}
 })
